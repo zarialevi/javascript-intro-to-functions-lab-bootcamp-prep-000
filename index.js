c27c09422a9c156ec6d27428c39869c5d090bfc1
zaria rankine <zarialevi@gmail.com>
2019-09-04 17:50:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return string
+}
+
+function logWhisper(string) {
+  console.log.toLowerCase();
+}
+
+function logShout(string) {
+  console.log();
+}
